2a0b1b31b0ee5f054ae1dcfa0b4d0faec298e2cd
Musifunk <clementcochie@gmail.com>
2026-05-18 19:27:18 +0200

    Remove privileged Discord message content intent

--- index.ts
+++ index.ts
@@ -5,11 +5,7 @@ export const bot = new Bot(
   new Client({
     intents: [
       GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildVoiceStates,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.GuildMessageReactions,
-      GatewayIntentBits.MessageContent,
-      GatewayIntentBits.DirectMessages
+      GatewayIntentBits.GuildVoiceStates
     ]
   })
 );
